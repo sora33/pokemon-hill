e347c046015991b2fb80f1dfc358a3d74767b364
Hiranuma Shuya(ひらぬま) <shuuuya0616@gmail.com>
2024-10-04 21:01:44 +0900

    修正

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,15 +1,15 @@
-// import type { NextRequest } from "next/server";
-// import { prisma } from "~/lib/prisma";
+import type { NextRequest } from "next/server";
+import { prisma } from "~/lib/prisma";
 
-// export async function GET(req: NextRequest) {
-// 	const authHeader = req.headers.get("authorization");
-// 	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-// 		return new Response("Unauthorized", {
-// 			status: 401,
-// 		});
-// 	}
+export async function GET(req: NextRequest) {
+	const authHeader = req.headers.get("authorization");
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response("Unauthorized", {
+			status: 401,
+		});
+	}
 
-// 	const seminarCategories = await prisma.seminarCategory.findFirst();
+	// const seminarCategories = await prisma.seminarCategory.findFirst();
 
-// 	return Response.json({ seminarCategories });
-// }
+	return Response.json({ message: "Hello World" });
+}
